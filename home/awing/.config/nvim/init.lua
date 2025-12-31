929a93e491c8af1c8e67a872d10ff23dd15d0760
Awing Ding <awingding.win@gmail.com>
2025-12-31 19:29:00 +0100

    thingies

--- home/awing/.config/nvim/init.lua
+++ home/awing/.config/nvim/init.lua
@@ -1,5 +1,2 @@
-require("config")
-vim.opt["tabstop"] = 4
-vim.opt["shiftwidth"] = 4
-vim.wo.relativenumber = true
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- home/awing/.config/nvim/lua/plugins/plugins.lua
+++ home/awing/.config/nvim/lua/plugins/plugins.lua
@@ -1,38 +1,3 @@
 return {
-	'mbbill/undotree',
-	'ThePrimeagen/harpoon',
-	'github/copilot.vim',
-	'stevearc/vim-arduino',
-	"mason-org/mason.nvim",
-	{
-		'chomosuke/typst-preview.nvim',
-		lazy = false,
-		version = '1.*',
-		opts = {}
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"mason.nvim",
-			"mason-org/mason-lspconfig.nvim",
-		},
-		opts = {
-			servers = {
-				tinymist = {
-					-- Fix semantic highlighting stuff when using non-ascii characters
-					offset_encoding = "utf-32",
-					single_file_support = true,
-					root_dir = function()
-						return vim.fn.getcwd()
-					end,
-					settings = {
-						formatterMode = "typstyle",
-            			exportPdf = "onSave",
-						-- formatterMode = "typstyle",
-					},
-					-- capabilities = default_capabilities,
-				},
-			},
-		},
-	},
+  "github/copilot.vim",
 }
